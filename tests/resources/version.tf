1ab30f16b228bcccd8b5c6fda5dbe1ac10df9b27
aashiq.jacob@ibm.com <aashiq.jacob@ibm.com>
2025-06-25 10:36:27 +0530

    review changes.

--- tests/resources/version.tf
+++ tests/resources/version.tf
@@ -4,7 +4,7 @@ terraform {
   required_providers {
     ibm = {
       source  = "ibm-cloud/ibm"
-      version = ">= 1.78.2"
+      version = ">= 1.79.2"
     }
     tls = {
       source  = "hashicorp/tls"
