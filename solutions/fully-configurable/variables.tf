e30038434edf8b5cb203655aef7ef5610ca49230
whoffler <WHoffler@ie.ibm.com>
2025-08-07 07:35:19 +0100

    chore: update existing_resource_group_name default to null

--- solutions/fully-configurable/variables.tf
+++ solutions/fully-configurable/variables.tf
@@ -10,8 +10,8 @@ variable "ibmcloud_api_key" {
 
 variable "existing_resource_group_name" {
   type        = string
-  description = "The name of an existing resource group in which to provision the private path services in."
-  default     = "Default"
+  description = "The name of an existing resource group to provision the resources. If not provided the default resource group will be used."
+  default     = null
 }
 
 variable "region" {
