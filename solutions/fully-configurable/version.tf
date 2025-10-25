30aef09741c52458e32b952929a5f8ed6d7bd75f
Terraform IBM Modules Operations <106112202+terraform-ibm-modules-ops@users.noreply.github.com>
2025-10-25 04:22:11 +0100

    fix(deps): update dependencies (#55)

--- solutions/fully-configurable/version.tf
+++ solutions/fully-configurable/version.tf
@@ -4,7 +4,7 @@ terraform {
     # Lock deployable architecture into an exact provider version - renovate automation will keep it updated
     ibm = {
       source  = "ibm-cloud/ibm"
-      version = "1.84.1"
+      version = "1.84.3"
     }
   }
 }
