f7ce67fa1651f6ac00f98fc6c2b3a7303d0485fc
aashiq.jacob@ibm.com <aashiq.jacob@ibm.com>
2025-12-19 13:50:35 +0530

    update secret manager

--- tests/pr_test.go
+++ tests/pr_test.go
@@ -293,7 +293,7 @@ func TestAddonsDefaultConfiguration(t *testing.T) {
 			OfferingName:   "deploy-arch-ibm-secrets-manager",
 			OfferingFlavor: "fully-configurable",
 			Inputs: map[string]interface{}{
-				"existing_secrets_manager_crn":         permanentResources["privateOnlySecMgrCRN"],
+				"existing_secrets_manager_crn":         permanentResources["secretsManagerCRN"],
 				"service_plan":                         "__NULL__", // no plan value needed when using existing SM
 				"skip_secrets_manager_iam_auth_policy": true,       // since using an existing Secrets Manager instance, attempting to re-create auth policy can cause conflicts if the policy already exists
 				"secret_groups":                        []string{}, // passing empty array for secret groups as default value is creating general group and it will cause conflicts as we are using an existing SM
