a85bdc09f10b361e98dd4f12b5c0598741729a02
Terraform IBM Modules Operations <106112202+terraform-ibm-modules-ops@users.noreply.github.com>
2025-12-28 04:19:55 +0000

    fix(deps): update dependencies (#74)

--- solutions/fully-configurable/main.tf
+++ solutions/fully-configurable/main.tf
@@ -32,7 +32,7 @@ module "application_loadbalancer_listener_certificate_crn_parser" {
 module "secrets_manager_secret_group" {
   count                    = var.application_loadbalancer_listener_certificate_crn == null && var.existing_secrets_manager_secret_group_id == null ? 1 : 0
   source                   = "terraform-ibm-modules/secrets-manager-secret-group/ibm"
-  version                  = "1.3.32"
+  version                  = "1.3.34"
   region                   = module.existing_sm_crn_parser.region
   secrets_manager_guid     = module.existing_sm_crn_parser.service_instance
   secret_group_name        = (var.prefix != null && var.prefix != "") ? "${var.prefix}-cert-secret-group" : "cert-secret-group"
@@ -46,7 +46,7 @@ module "secrets_manager_secret_group" {
 module "secrets_manager_private_certificate" {
   count                  = var.application_loadbalancer_listener_certificate_crn == null ? 1 : 0
   source                 = "terraform-ibm-modules/secrets-manager-private-cert/ibm"
-  version                = "1.10.6"
+  version                = "1.10.9"
   cert_name              = (var.prefix != null && var.prefix != "") ? "${var.prefix}-cts-vpn-private-cert" : "cts-vpn-private-cert"
   cert_description       = "private certificate for client to site VPN connection"
   cert_template          = var.private_cert_engine_config_template_name
@@ -124,7 +124,7 @@ resource "ibm_is_lb_listener" "alb_frontend_listener" {
 
 module "private_path" {
   source            = "terraform-ibm-modules/vpc-private-path/ibm"
-  version           = "1.5.6"
+  version           = "1.5.9"
   resource_group_id = module.resource_group.resource_group_id
   subnet_id         = local.subnet_id
   tags              = var.private_path_tags
