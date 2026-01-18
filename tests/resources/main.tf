713aa1a81f63b9e63accb6e649b7fa74bf14945c
Terraform IBM Modules Operations <106112202+terraform-ibm-modules-ops@users.noreply.github.com>
2026-01-18 01:11:26 +0000

    fix(deps): update terraform terraform-ibm-modules/secrets-manager-private-cert/ibm to v1.10.16 (#85)

--- tests/resources/main.tf
+++ tests/resources/main.tf
@@ -148,7 +148,7 @@ module "secrets_manager_group" {
 module "secrets_manager_private_certificate" {
   count                  = var.existing_secrets_manager_instance_crn != null ? 1 : 0
   source                 = "terraform-ibm-modules/secrets-manager-private-cert/ibm"
-  version                = "1.10.15"
+  version                = "1.10.16"
   cert_name              = "${var.prefix}-cts-vpn-private-cert"
   cert_description       = "an example private cert"
   cert_template          = var.certificate_template_name
