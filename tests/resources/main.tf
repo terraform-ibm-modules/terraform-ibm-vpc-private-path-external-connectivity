3d88bef637eb792b420fcfdf501b886c7e0e55d6
Terraform IBM Modules Operations <106112202+terraform-ibm-modules-ops@users.noreply.github.com>
2026-07-03 16:54:22 +0100

    fix(deps): update dependencies (#113)

--- tests/resources/main.tf
+++ tests/resources/main.tf
@@ -4,7 +4,7 @@
 
 module "mock_resource_group" {
   source              = "terraform-ibm-modules/resource-group/ibm"
-  version             = "1.6.0"
+  version             = "1.6.1"
   resource_group_name = "${var.prefix}-mock-rg"
 }
 
@@ -81,7 +81,7 @@ resource "ibm_is_floating_ip" "ip" {
 ################################################################################
 module "resource_group" {
   source              = "terraform-ibm-modules/resource-group/ibm"
-  version             = "1.6.0"
+  version             = "1.6.1"
   resource_group_name = "${var.prefix}-rg"
 }
 
@@ -126,7 +126,7 @@ locals {
 module "existing_sm_crn_parser" {
   count   = var.existing_secrets_manager_instance_crn != null ? 1 : 0
   source  = "terraform-ibm-modules/common-utilities/ibm//modules/crn-parser"
-  version = "1.5.0"
+  version = "1.9.0"
   crn     = var.existing_secrets_manager_instance_crn
 }
 
@@ -148,7 +148,7 @@ module "secrets_manager_group" {
 module "secrets_manager_private_certificate" {
   count                  = var.existing_secrets_manager_instance_crn != null ? 1 : 0
   source                 = "terraform-ibm-modules/secrets-manager-private-cert/ibm"
-  version                = "1.12.2"
+  version                = "1.12.4"
   cert_name              = "${var.prefix}-cts-vpn-private-cert"
   cert_description       = "an example private cert"
   cert_template          = var.certificate_template_name
